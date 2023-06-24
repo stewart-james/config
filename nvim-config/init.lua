fd3bd998f803451fb98fdcfc994a346b08305c09
stew <contact@stew.dev>
2023-06-24 09:03:41 +0100

    rename nvim-config to nvim

--- nvim-config/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require('packages')
-require('languages')
-require('autocomplete')
-require('config')
-require('keymaps')
-require('appearance')
---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('packages')
+require('languages')
+require('autocomplete')
+require('config')
+require('keymaps')
+require('appearance')
