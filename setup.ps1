b1ecf553fca3ff9072c939ac85ca6a8c178f54cc
Stewart James <contact@stew.dev>
2023-06-27 22:51:12 +0100

    add git config to setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,15 @@
+
+# Symbolic Links
+# ----------------
 #New-Item -ItemType HardLink -Path "$HOME\.ideavimrc" -Target "c:\projects\apps\config\rider\.ideavimrc"
 #New-Item -ItemType HardLink -Path "$HOME\ohmyposh.json" -Target "c:\projects\apps\config\ohmyposh\ohmyposh.json"
 
 New-Item -ItemType HardLink -Path "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "c:\projects\apps\config\wt\settings.json"
 New-Item -ItemType HardLink -Path "$HOME\.glaze-wm\config.yaml" -Target "c:\projects\apps\config\glazewm\config.yaml"
+
+# git config
+# ----------------
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
