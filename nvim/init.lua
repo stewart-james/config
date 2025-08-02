2ea4fe4f293e70f0b712a7725f911a43351ce2e6
Stewart James <contact@stew.dev>
2025-08-02 11:11:01 +0100

    add luasnip

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,6 +36,15 @@ vim.pack.add(
 		-- keymaps
 		{ src = "https://github.com/folke/which-key.nvim" },
 
+		-- auto completion
+		{ src = "https://github.com/hrsh7th/nvim-cmp" },
+		{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+		{ src = "https://github.com/saadparwaiz1/cmp_luasnip" },
+
+		-- snippets
+		{ src = "https://github.com/L3MON4D3/LuaSnip" },
+		{ src = "https://github.com/rafamadriz/friendly-snippets" },
+
 		-- language servers
 		{
 			src = "https://github.com/nvim-treesitter/nvim-treesitter",
@@ -52,6 +61,55 @@ vim.pack.add(
 		{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 		{ src = "https://github.com/kkoomen/vim-doge" },
 	});
+
+	local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+}
+
+require("snippets")
 
 require("nvim-tree").setup {}
 require("todo-comments").setup {
@@ -215,3 +273,5 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.cmd("colorscheme catppuccin")
+
+vim.diagnostic.config({ virtual_lines = true })
