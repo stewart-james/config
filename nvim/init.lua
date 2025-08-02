0e6063bf10222b7ef78acb7a9eb559bebeb019db
Stewart James <contact@stew.dev>
2025-08-02 12:17:08 +0100

    add snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,7 +43,6 @@ vim.pack.add(
 
 		-- snippets
 		{ src = "https://github.com/L3MON4D3/LuaSnip" },
-		{ src = "https://github.com/rafamadriz/friendly-snippets" },
 
 		-- language servers
 		{
@@ -62,51 +61,52 @@ vim.pack.add(
 		{ src = "https://github.com/kkoomen/vim-doge" },
 	});
 
-	local cmp = require('cmp')
+local cmp = require('cmp')
 local luasnip = require('luasnip')
 
-require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load {
+	exclude = { "csharp" } }
 luasnip.config.setup {}
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert {
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete {},
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	},
 }
 
 require("snippets")
@@ -207,9 +207,9 @@ end
 vim.cmd([[tnoremap <Esc> <C-\><C-n>:call feedkeys("\<C-c>")<CR>]])
 
 require('telescope').setup({
-  defaults = {
-    path_display = { "smart" }
-  }
+	defaults = {
+		path_display = { "smart" }
+	}
 })
 
 require("which-key").add(groups)
--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -2,8 +2,47 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local extras = require("luasnip.extras")
+local r = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
 
-ls.add_snippets("lua", { s("hello", {
-	t('print("hello world")')
-})
-})
+local csharp_snippets =
+{
+	s("exception", fmt(
+		[[
+		/// <Summary>
+		///	An exception that is thrown when {}.
+		/// </Summary>
+		public class {}Exception : {}Exception
+		{{
+			/// <Summary>
+			///	Initializes a new instance of the <see cref="{}Exception"/> class.
+			/// </Summary>
+			public {}Exception()
+			{{
+			}}
+
+			/// <Summary>
+			///	Initializes a new instance of the <see cref="{}Exception"/> class with a specified error message.
+			/// </Summary>
+			/// <paramref name="message">The error message that explains the reason for the exception.</paramref>
+			public {}Exception(string message)
+				: base(message)
+			{{
+			}}
+
+			/// <Summary>
+			///	Initializes a new instance of the <see cref="{}Exception"/> class with a specified error message
+			/// and a reference to the inner exception that is the cause of this exception.
+			/// </Summary>
+			/// <paramref name="message">The error message that explains the reason for the exception.</paramref>
+			/// <paramref name="innerException">The exception that is the cause of the current exception.</paramref>
+			public {}Exception(string message, Exception? innerException)
+				:base(message, innerException)
+			{{
+			}}
+		}}
+		]], { i(3), i(1), i(2), r(1), r(1), r(1), r(1), r(1), r(1) }))
+}
+
+ls.add_snippets("cs", csharp_snippets)
