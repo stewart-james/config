d2d72e3b00aaaef20de0cb4e547fbcd7723377a1
Stewart James <contact@stew.dev>
2025-08-08 17:25:16 +0100

    setup vim runtime file location

--- nvim/init.lua
+++ nvim/init.lua
@@ -222,6 +222,13 @@ local lsps =
 		cmd = { 'lua-language-server' },
 		filetypes = { 'lua' },
 		root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
+		settings = {
+			Lua = {
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+				}
+			}
+		}
 	},
 
 	roslyn = {
